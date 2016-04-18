52924673e46aa49f91d58527d63326e85c3c3dda
Jake Steinberg and Kevin Mattison <jakesteinberg@gmail.com>
2016-04-18 15:08:02 -0700

    add new blog post feature

--- app/src/main/java/com/epicodus/myspace/profileActivity.java
+++ app/src/main/java/com/epicodus/myspace/profileActivity.java
@@ -1,13 +1,37 @@
 package com.epicodus.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class profileActivity extends AppCompatActivity {
+    private Button mblogButton;
+    private EditText minputBlog;
+    private TextView mBlogTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        minputBlog = (EditText) findViewById(R.id.blogInput);
+        mblogButton = (Button) findViewById(R.id.blogButton);
+        mBlogTextView = (TextView) findViewById(R.id.BlogTextView);
+        mblogButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String blog = minputBlog.getText().toString();
+//                Intent intent = new Intent(profileActivity.this, profileActivity.class);
+//                intent.putExtra("blog", blog);
+                mBlogTextView.setText(blog);
+                //startActivity(intent);
+
+
+            }
+        });
     }
 }
