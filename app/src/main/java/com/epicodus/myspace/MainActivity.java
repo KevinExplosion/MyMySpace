6e7feace3e1a57bdcefe6d3f4a2ea1163fc7825e
Jake Steinberg and Kevin Mattison <jakesteinberg@gmail.com>
2016-04-18 11:48:10 -0700

    Create picture class.

--- app/src/main/java/com/epicodus/myspace/MainActivity.java
+++ app/src/main/java/com/epicodus/myspace/MainActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.myspace;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mpictureButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mpictureButton = (Button) findViewById(R.id.pictureButton);
+        mpictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, pictureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
